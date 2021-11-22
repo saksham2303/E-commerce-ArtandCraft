661a6c50e6fff48add18b11e8515f14c6a3a3ee1
saksham2303 <sakshambairathi@gmail.com>
2021-11-22 11:09:54 +0530

    done

--- e-commerce/src/components/Carousel.js
+++ e-commerce/src/components/Carousel.js
@@ -23,7 +23,7 @@ function Carousel() {
   <ReactBootstrap.Carousel.Item interval={2500}>
     <img
       className="d-block w-100"
-      src="http://localhost:5001/png/s1.jpg"
+      src="http://localhost:5001/png/artist-1628075847179.png"
      
       alt="Second slide"
       style={{height:"500px"}}
@@ -36,7 +36,7 @@ function Carousel() {
   <ReactBootstrap.Carousel.Item interval={2500}>
     <img
       className="d-block w-100"
-      src="http://localhost:5001/png/s2.jpg"
+      src="http://localhost:5001/png/istockphoto-1263478176-170667a.jpg"
       alt="Third slide"
       style={{height:"500px"}}
       
@@ -51,7 +51,7 @@ function Carousel() {
   <ReactBootstrap.Carousel.Item interval={2500}>
   <img
       className="d-block w-100"
-      src="http://localhost:5001/png/s3.png"
+      src="http://localhost:5001/png/923d9c04-cd8d-4873-b5fe-95c92c56f8f5.jpg"
      
       alt="Second slide"
       style={{height:"500px"}}
@@ -64,7 +64,7 @@ function Carousel() {
   <ReactBootstrap.Carousel.Item interval={2500}>
   <img
       className="d-block w-100"
-      src="http://localhost:5001/png/s4.webp"
+      src="http://localhost:5001/png/ukraine-matryoshka-nesting-doll-russian-doll.jpg"
      
       alt="Second slide"
       style={{height:"500px"}}
@@ -75,7 +75,7 @@ function Carousel() {
     </ReactBootstrap.Carousel.Caption>
   </ReactBootstrap.Carousel.Item>
 
-  <ReactBootstrap.Carousel.Item interval={2500}>
+  {/* <ReactBootstrap.Carousel.Item interval={2500}>
   <img
       className="d-block w-100"
       src="http://localhost:5001/png/s5.jpg"
@@ -87,7 +87,7 @@ function Carousel() {
       <h3>Third slide </h3>
       <p>Teesri image</p>
     </ReactBootstrap.Carousel.Caption>
-  </ReactBootstrap.Carousel.Item>
+  </ReactBootstrap.Carousel.Item> */}
   <ReactBootstrap.Carousel.Item interval={2500}>
   <img
       className="d-block w-100"
